5a71ae02c2740f64c2769f83fdf72992c1517ce8
Luffy211 <892442464@qq.com>
2020-06-24 13:38:29 +0800

    2020/6/24

--- src/MyStructure/MyQueue/ArrayQueue.java
+++ src/MyStructure/MyQueue/ArrayQueue.java
@@ -6,11 +6,13 @@ package MyStructure.MyQueue;
 //2、填满数组，但利用一个标志位sign，入队时令sign=1，出队时令sign=0，如果sign==0 且头指针和尾指针指向同一个，则判断为空；为满时相对的sign==1
 //3、填满数组，使用count计数，入队时count++，出队时count--
 public class ArrayQueue {
-    int[] array;
-    static int MaxSize;
-    int head;
-    int rear;
-
+    private int[] array;
+    static int MaxSize = 6;
+    private int head;
+    private int rear;
+    private int size;
+    private int sign;
+    /*
     //方法1：
     ArrayQueue(){
         this.array = new int[MaxSize];
@@ -25,8 +27,119 @@ public class ArrayQueue {
 
     //入队列,需要先判断队列是否满了
     public void push(int val){
-
+        if((rear+1)%MaxSize == head){
+            return;
+        }
+        array[rear] = val;
+        rear = (rear+1)%MaxSize;
     }
 
-    //出队列，需要判断队列
+    //出队列，需要判断队列是否为空
+    public boolean pop(){
+        if(head==rear){
+            return false;
+        }
+        int x = array[head];
+        head = (head+1)%MaxSize;
+        return true;
+    }
+
+    //计算队列内元素的个数
+    public int getSize(){
+        return (rear+MaxSize-head)%MaxSize;
+    }*/
+    //方法2：结合sign判断为空或为满
+    /*
+    ArrayQueue(){
+        this.array = new int[MaxSize];
+        this.head=0;
+        this.rear=0;
+        this.sign=0;
+    }
+
+    public boolean isEmpty(){
+        if(head==rear&&sign==0)
+            return true;
+        return false;
+    }
+
+    public void push(int val){
+        if(head==rear&&sign==1){        //队满
+            return;
+        }
+        array[rear] = val;
+        rear = (rear+1)%MaxSize;
+        sign = 1;
+    }
+
+    public boolean pop(){
+        if(head==rear&&sign==0){
+            return false;
+        }
+        int x = array[head];
+        head = (head+1)%MaxSize;
+        sign = 0;
+        return true;
+    }
+
+    public int getSize(){
+        if(sign==1&&rear==head)
+            return MaxSize;
+        else if(sign==0&&rear==head)
+            return 0;
+        else return (rear+MaxSize-head)%MaxSize;
+    }*/
+    //方法3：利用变量size 记录当前队列元素个数
+    ArrayQueue(){
+        this.array = new int[MaxSize];
+        this.rear = 0;
+        this.head = 0;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){
+        if(size==0)
+            return true;
+        return false;
+    }
+    public boolean push(int val){
+        if(size==MaxSize){
+            return false;
+        }
+        array[rear] = val;
+        rear = (rear+1)%MaxSize;
+        size++;
+        return true;
+    }
+    public boolean pop(){
+        if(size==0)
+            return false;
+        int x = array[head];
+        head = (head+1)%MaxSize;
+        size--;
+        return true;
+    }
+    public int getSize(){
+        return size;
+    }
+    public static void main(String[] args) {
+        ArrayQueue q = new ArrayQueue();
+        System.out.println(q.isEmpty());
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+        q.push(6);
+        System.out.println(q.getSize());
+        q.push(7);
+        System.out.println(q.getSize());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+    }
 }
--- src/MyStructure/MyQueue/LinkedListQueue.java
+++ src/MyStructure/MyQueue/LinkedListQueue.java
@@ -1,8 +1,87 @@
 package MyStructure.MyQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
+//使用双端队列实现
 public class LinkedListQueue {
+    private class Node{
+        int value;
+        Node next;
+        Node(){
+            value = 0;
+            next = null;
+        }
+    }
+    Node head;
+    Node rear;
+    //初始化
+    LinkedListQueue(){
+        this.head=null;
+        this.rear=null;
+    }
+    //判断是否为空
+    public boolean isEmpty(){
+        return head==null&&rear==null;
+    }
+    //入队列
+    public void push(int val){
+        Node newNode = new Node();
+        newNode.value = val;
+        if(head==null&&rear==null){
+            head = newNode;         //队列初始为空时 入队特殊处理
+            rear = newNode;
+        }else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+    //出队列
+    public boolean pop(){
+        if(head==null&&rear==null)
+            return false;
+        int x = head.value;
+        if(head==rear){             //队列只有一个节点时 出队特殊处理
+            head=null;
+            rear=null;
+        }else {
+            head = head.next;
+        }
+        return true;
+    }
+    //求队列长
+    public int getSize(){
+        if(head==null&&rear==null)
+            return 0;
+        int cnt=0;
+        Node tmp = head;
+        while(tmp!=null){
+            tmp = tmp.next;
+            cnt++;
+        }
+        return cnt;
+    }
 
-
-
+    public static void main(String[] args) {
+        /*
+        LinkedListQueue q = new LinkedListQueue();
+        System.out.println(q.isEmpty());
+        q.push(1);
+        q.push(2);
+        System.out.println(q.isEmpty());
+        System.out.println(q.getSize());
+        q.pop();
+        q.push(3);
+        System.out.println(q.getSize());
+        q.pop();
+        q.pop();
+        System.out.println(q.isEmpty());*/
+        //JAVA内置队列：
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(1);         //入队列
+        q.offer(2);
+        System.out.println(q.poll());               //出队
+        System.out.println(q.peek());               //返回队列第一个元素但不会队
+        System.out.println(q.size());               //返回元素个数
+    }
 }
